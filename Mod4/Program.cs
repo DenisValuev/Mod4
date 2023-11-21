a22691d68da59f012b6a1e78bdf78a6e268a2ab3
DenisValuev <valuev.denis91@mail.ru>
2023-11-21 16:40:55 +0300

    Mod4-branch1:Добавлено заадние 4.1.5 сравнение целых и вещественных чисел

--- Mod4/Program.cs
+++ Mod4/Program.cs
@@ -8,7 +8,15 @@
             string A = "abc";
             string B = "cba";
             Boolean C = A != B;
-            Console.WriteLine(C);
+            Console.WriteLine("Задание 4.1.4 Ответ: {0}", C);
+
+            //Задание 4.1.5
+            int A1 = 3;
+            int B1 = 4;
+            double X = 2;
+            double Y = 1;
+            bool C1 = A1 < B1 | X > Y;
+            Console.WriteLine("Задание 4.1.5 Ответ: {0}", C1);
         }
     }
 }
