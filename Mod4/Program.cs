cc81cd4a8f449979bb2a45ff420f69dae091903b
DenisValuev <valuev.denis91@mail.ru>
2023-11-23 14:32:06 +0300

    Mod4-branch1:Добавлен цикл for

--- Mod4/Program.cs
+++ Mod4/Program.cs
@@ -77,6 +77,40 @@
                     Console.WriteLine("Ваш цвет желтый");
                     break;
             }
+
+            //Цикл For
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Итерация {0}", i);
+                switch(Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш цвет красный");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш цвет зеленый");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Ваш цвет бирюзовый");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ваш цвет желтый");
+                        break;
+                    }
+                }
+
+            }
         }
     }
 }
